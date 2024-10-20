3adec5a1d825ea857fad1e4b4fe73478ff60dbe9
vmagnin <vincent.magnin@libertysurf.fr>
2024-10-20 21:37:34 +0200

    src/grapheme_class.f90 renamed src/charac_class.f90

--- saville_code/build.sh
+++ saville_code/build.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 # Vincent Magnin, 2023-03-26
-# Last modification: 2023-04-21
+# Last modification: 2024-10-20
 # Verified with shellcheck
 
 # Strict mode:
@@ -19,7 +19,7 @@ else
   FLAGS="-Wall -Wextra -Wno-unused-dummy-argument -fcheck=all -std=f2008 -pedantic"
 fi
 
-"${FC}" ${FLAGS} src/color_class.f90 src/glyph_class.f90 src/grapheme_class.f90 src/gui.f90 app/saville_code.f90 $(pkg-config --cflags --libs gtk-4-fortran)
+"${FC}" ${FLAGS} src/color_class.f90 src/glyph_class.f90 src/charac_class.f90 src/gui.f90 app/saville_code.f90 $(pkg-config --cflags --libs gtk-4-fortran)
 
 # Shows the executable:
 ls -oh a.out
